9f537051aa0027fd162047361630219500fe782c
Kanabenki <lucien.menassol@gmail.com>
2024-01-30 14:21:59 +0100

    Use a explicit stack instead of recursion for propagating transforms

--- crates/bevy_transform/src/systems.rs
+++ crates/bevy_transform/src/systems.rs
@@ -1,3 +1,7 @@
+use std::cell::Cell;
+
+use thread_local::ThreadLocal;
+
 use crate::components::{GlobalTransform, Transform};
 use bevy_ecs::{
     change_detection::Ref,
@@ -55,6 +59,7 @@ pub fn propagate_transforms(
     transform_query: Query<(Ref<Transform>, &mut GlobalTransform, Option<&Children>), With<Parent>>,
     parent_query: Query<(Entity, Ref<Parent>)>,
     mut orphaned_entities: Local<Vec<Entity>>,
+    thread_stacks: Local<ThreadLocal<Cell<Vec<(Entity, GlobalTransform, bool)>>>>,
 ) {
     orphaned_entities.clear();
     orphaned_entities.extend(orphaned.read());
@@ -66,116 +71,86 @@ pub fn propagate_transforms(
                 *global_transform = GlobalTransform::from(*transform);
             }
 
+            // TODO(kanabenki): update safety comments
+            let stack_cell = thread_stacks.get_or_default();
+            let mut stack = stack_cell.take();
+
             for (child, actual_parent) in parent_query.iter_many(children) {
                 assert_eq!(
                     actual_parent.get(), entity,
                     "Malformed hierarchy. This probably means that your hierarchy has been improperly maintained, or contains a cycle"
                 );
+                stack.clear();
+                stack.push((child, *global_transform, changed));
+                while let Some((entity, parent, mut changed)) = stack.pop() {
+                    let (global_matrix, children) = {
+                        let Ok((transform, mut global_transform, children)) =
+                            // SAFETY: This call cannot create aliased mutable references.
+                            //   - The top level iteration parallelizes on the roots of the hierarchy.
+                            //   - The caller ensures that each child has one and only one unique parent throughout the entire
+                            //     hierarchy.
+                            //
+                            // For example, consider the following malformed hierarchy:
+                            //
+                            //     A
+                            //   /   \
+                            //  B     C
+                            //   \   /
+                            //     D
+                            //
+                            // D has two parents, B and C. If the propagation passes through C, but the Parent component on D points to B,
+                            // the above check will panic as the origin parent does match the recorded parent.
+                            //
+                            // Also consider the following case, where A and B are roots:
+                            //
+                            //  A       B
+                            //   \     /
+                            //    C   D
+                            //     \ /
+                            //      E
+                            //
+                            // Even if these A and B start two separate tasks running in parallel, one of them will panic before attempting
+                            // to mutably access E.
+                            (unsafe { transform_query.get_unchecked(entity) }) else {
+                                continue;
+                            };
+
+                        changed |= transform.is_changed() || global_transform.is_added();
+                        if changed {
+                            *global_transform = parent.mul_transform(*transform);
+                        }
+                        (*global_transform, children)
+                    };
+
+                    let Some(children) = children else { continue };
+                    for (child, actual_parent) in parent_query.iter_many(children) {
+                        assert_eq!(
+                            actual_parent.get(), entity,
+                            "Malformed hierarchy. This probably means that your hierarchy has been improperly maintained, or contains a cycle"
+                        );
+                        // SAFETY: The caller guarantees that `transform_query` will not be fetched
+                        // for any descendants of `entity`, so it is safe to call `propagate_recursive` for each child.
+                        //
+                        // The above assertion ensures that each child has one and only one unique parent throughout the
+                        // entire hierarchy.
+                        stack.push((child, global_matrix, changed || actual_parent.is_changed()));
+                    }
+                }
+
                 // SAFETY:
                 // - `child` must have consistent parentage, or the above assertion would panic.
                 // Since `child` is parented to a root entity, the entire hierarchy leading to it is consistent.
-                // - We may operate as if all descendants are consistent, since `propagate_recursive` will panic before 
+                // - We may operate as if all descendants are consistent, since `propagate_recursive` will panic before
                 //   continuing to propagate if it encounters an entity with inconsistent parentage.
                 // - Since each root entity is unique and the hierarchy is consistent and forest-like,
                 //   other root entities' `propagate_recursive` calls will not conflict with this one.
                 // - Since this is the only place where `transform_query` gets used, there will be no conflicting fetches elsewhere.
-                unsafe {
-                    propagate_recursive(
-                        &global_transform,
-                        &transform_query,
-                        &parent_query,
-                        child,
-                        changed || actual_parent.is_changed(),
-                    );
-                }
+
             }
-        },
+
+            stack_cell.set(stack);
+        }
     );
-}
-
-/// Recursively propagates the transforms for `entity` and all of its descendants.
-///
-/// # Panics
-///
-/// If `entity`'s descendants have a malformed hierarchy, this function will panic occur before propagating
-/// the transforms of any malformed entities and their descendants.
-///
-/// # Safety
-///
-/// - While this function is running, `transform_query` must not have any fetches for `entity`,
-/// nor any of its descendants.
-/// - The caller must ensure that the hierarchy leading to `entity`
-/// is well-formed and must remain as a tree or a forest. Each entity must have at most one parent.
-unsafe fn propagate_recursive(
-    parent: &GlobalTransform,
-    transform_query: &Query<
-        (Ref<Transform>, &mut GlobalTransform, Option<&Children>),
-        With<Parent>,
-    >,
-    parent_query: &Query<(Entity, Ref<Parent>)>,
-    entity: Entity,
-    mut changed: bool,
-) {
-    let (global_matrix, children) = {
-        let Ok((transform, mut global_transform, children)) =
-            // SAFETY: This call cannot create aliased mutable references.
-            //   - The top level iteration parallelizes on the roots of the hierarchy.
-            //   - The caller ensures that each child has one and only one unique parent throughout the entire
-            //     hierarchy.
-            //
-            // For example, consider the following malformed hierarchy:
-            //
-            //     A
-            //   /   \
-            //  B     C
-            //   \   /
-            //     D
-            //
-            // D has two parents, B and C. If the propagation passes through C, but the Parent component on D points to B,
-            // the above check will panic as the origin parent does match the recorded parent.
-            //
-            // Also consider the following case, where A and B are roots:
-            //
-            //  A       B
-            //   \     /
-            //    C   D
-            //     \ /
-            //      E
-            //
-            // Even if these A and B start two separate tasks running in parallel, one of them will panic before attempting
-            // to mutably access E.
-            (unsafe { transform_query.get_unchecked(entity) }) else {
-                return;
-            };
-
-        changed |= transform.is_changed() || global_transform.is_added();
-        if changed {
-            *global_transform = parent.mul_transform(*transform);
-        }
-        (*global_transform, children)
-    };
-
-    let Some(children) = children else { return };
-    for (child, actual_parent) in parent_query.iter_many(children) {
-        assert_eq!(
-            actual_parent.get(), entity,
-            "Malformed hierarchy. This probably means that your hierarchy has been improperly maintained, or contains a cycle"
-        );
-        // SAFETY: The caller guarantees that `transform_query` will not be fetched
-        // for any descendants of `entity`, so it is safe to call `propagate_recursive` for each child.
-        //
-        // The above assertion ensures that each child has one and only one unique parent throughout the
-        // entire hierarchy.
-        unsafe {
-            propagate_recursive(
-                &global_matrix,
-                transform_query,
-                parent_query,
-                child,
-                changed || actual_parent.is_changed(),
-            );
-        }
-    }
 }
 
 #[cfg(test)]
